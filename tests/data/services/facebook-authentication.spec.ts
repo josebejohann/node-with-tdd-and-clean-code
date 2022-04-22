b1e8907d31a8bedfea780725da424cc296b33df9
josebejohann <josebernardojohann@gmail.com>
2022-04-21 22:58:40 -0300

    Simplifies code using Intersection Types

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -6,34 +6,29 @@ import { mock, MockProxy } from 'jest-mock-extended'
 
 type SutTypes = {
   sut: FacebookAuthenticationService
-  loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
-  loadUserAccountRepository: MockProxy<LoadUserAccountRepository>
-  createFacebookAccountRepository: MockProxy<CreateFacebookAccountRepository>
+  facebookApi: MockProxy<LoadFacebookUserApi>
+  userAccountRepository: MockProxy<LoadUserAccountRepository & CreateFacebookAccountRepository>
 }
 
 const makeSut = (): SutTypes => {
-  const loadFacebookUserApi = mock<LoadFacebookUserApi>()
-  loadFacebookUserApi.loadUser.mockResolvedValue({
+  const facebookApi = mock<LoadFacebookUserApi>()
+  facebookApi.loadUser.mockResolvedValue({
     facebookId: 'any_facebook_id',
     email: 'any_facebook_email',
     name: 'any_facebook_name'
   })
 
-  const loadUserAccountRepository = mock<LoadUserAccountRepository>()
-
-  const createFacebookAccountRepository = mock<CreateFacebookAccountRepository>()
+  const userAccountRepository = mock<LoadUserAccountRepository & CreateFacebookAccountRepository>()
 
   const sut = new FacebookAuthenticationService(
-    loadFacebookUserApi,
-    loadUserAccountRepository,
-    createFacebookAccountRepository
+    facebookApi,
+    userAccountRepository
   )
 
   return {
     sut,
-    loadFacebookUserApi,
-    loadUserAccountRepository,
-    createFacebookAccountRepository
+    facebookApi,
+    userAccountRepository
   }
 }
 
@@ -41,18 +36,18 @@ describe('FacebookAuthenticationService', () => {
   const token = 'any_token'
 
   it('should call LoadFacebookUserApi with correct params', async () => {
-    const { sut, loadFacebookUserApi } = makeSut()
+    const { sut, facebookApi } = makeSut()
 
     await sut.perform({ token })
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token })
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
+    expect(facebookApi.loadUser).toHaveBeenCalledWith({ token })
+    expect(facebookApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    const { sut, loadFacebookUserApi } = makeSut()
+    const { sut, facebookApi } = makeSut()
 
-    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
+    facebookApi.loadUser.mockResolvedValueOnce(undefined)
 
     const authResult = await sut.perform({ token })
 
@@ -60,26 +55,26 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('should call LoadUserAccountRepository when LoadFacebookUserApi returns data', async () => {
-    const { sut, loadUserAccountRepository } = makeSut()
+    const { sut, userAccountRepository } = makeSut()
 
     await sut.perform({ token })
 
-    expect(loadUserAccountRepository.load).toHaveBeenCalledWith({ email: 'any_facebook_email' })
-    expect(loadUserAccountRepository.load).toHaveBeenCalledTimes(1)
+    expect(userAccountRepository.load).toHaveBeenCalledWith({ email: 'any_facebook_email' })
+    expect(userAccountRepository.load).toHaveBeenCalledTimes(1)
   })
 
   it('should call CreateUserAccountRepository when LoadUserAccountRepository returns undefined', async () => {
-    const { sut, loadUserAccountRepository, createFacebookAccountRepository } = makeSut()
+    const { sut, userAccountRepository } = makeSut()
 
-    loadUserAccountRepository.load.mockResolvedValueOnce(undefined)
+    userAccountRepository.load.mockResolvedValueOnce(undefined)
 
     await sut.perform({ token })
 
-    expect(createFacebookAccountRepository.createFromFacebook).toHaveBeenCalledWith({
+    expect(userAccountRepository.createFromFacebook).toHaveBeenCalledWith({
       facebookId: 'any_facebook_id',
       email: 'any_facebook_email',
       name: 'any_facebook_name'
     })
-    expect(createFacebookAccountRepository.createFromFacebook).toHaveBeenCalledTimes(1)
+    expect(userAccountRepository.createFromFacebook).toHaveBeenCalledTimes(1)
   })
 })
