3aa03262892a2db5cb1651156c7afab64772b8f0
josebejohann <josebernardojohann@gmail.com>
2022-04-23 22:51:47 -0300

    Ensures JwtTokenGenerator rethrows if sign throws

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -50,4 +50,14 @@ describe('JwtTokenGenerator', () => {
 
     expect(token).toBe('any_token')
   })
+
+  it('should rethrow if sign throws', async () => {
+    const { sut, fakeJwt } = makeSut()
+
+    fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error') })
+
+    const promise = sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+
+    await expect(promise).rejects.toThrow(new Error('token_error'))
+  })
 })
