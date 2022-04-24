b25d42236de6ef9cefcb174fba4d5e5f9da6019d
josebejohann <josebernardojohann@gmail.com>
2022-04-23 21:21:40 -0300

    Ensures FacebookApi gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -11,9 +11,11 @@ type SutTypes = {
 }
 
 const makeSut = (): SutTypes => {
-  const httpClient = mock<HttpGetClient>()
   const clientId = 'any_client_id'
   const clientSecret = 'any_client_secret'
+
+  const httpClient = mock<HttpGetClient>()
+  httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
 
   const sut = new FacebookApi(
     httpClient,
@@ -41,6 +43,20 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  it('should get debug token', async () => {
+    const { sut, httpClient } = makeSut()
+
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
